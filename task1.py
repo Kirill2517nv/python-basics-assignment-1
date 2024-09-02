1642f54214a29fd5106415185724ed0090503783
Kirill <kirill2517nv@gmail.com>
2024-09-02 11:23:43 +0700

    Started doing tasks

---
+++ task1.py
@@ -0,0 +1,24 @@
+# main.py
+
+def calculate_sum(a, b):
+    """
+    Эта функция принимает два числа и возвращает их сумму.
+
+    :param a: Первое число
+    :param b: Второе число
+    :return: Сумма a и b
+    """
+    return a + b
+
+
+def main():
+    # Пример использования функции calculate_sum
+    number1 = 5
+    number2 = 7
+    result = calculate_sum(number1, number2)
+
+    print(f"Сумма 12313  {number1} и {number2} равна {result}")
+
+
+if __name__ == "__main__":
+    main()
